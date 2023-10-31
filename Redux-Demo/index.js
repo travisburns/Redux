a887d5d6e8cd82444c8a32f0e13875e88ea12f59
Travis <travisjb0@gmail.com>
2023-10-30 20:03:28 -0700

    three core principles of redux

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,1 +1,11 @@
-console.log("From index.js")
+console.log("Store: that holds the state of your application")
+console.log("action: describes what happened in the application")
+console.log("Reducer: which handles the action and decides how to update the state")
+
+
+
+//Cake shop scenerio //Redux //Purpose
+//shop                Action   Describes what happened
+//shopkeeper          Reducer    ties the store and actions together
+
+
