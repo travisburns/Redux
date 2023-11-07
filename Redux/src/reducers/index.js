bba86d08dacfd14136439e50d30e7080ba6a58fb
Travis <travisjb0@gmail.com>
2023-11-07 12:46:22 -0800

    practiced basic reducer setup. 45% understanding estimated

--- Redux/src/reducers/index.js
+++ Redux/src/reducers/index.js
@@ -1,15 +1,55 @@
+// import characters_json from '../data/characters.json'
+// import { ADD_CHARACTER } from '../actions/index.js'
+
+// function characters(state = characters_json, action) {
+//   switch(action.type) {
+//     case ADD_CHARACTER:
+//       let characters = state.filter(item => item.id !== action.id);
+//       return characters;  
+//     default:
+//         return state;
+//   }  
+// }
+
+// export default characters;
+
+// import { ADD_CHARACTER } from '../actions';
+// import characters_json from '../data/characters.json'
+
+// function characters(state = characters_json, action) {
+//   switch(action.type) {
+//     case ADD_CHARACTER:
+//       default:
+//         return state; 
+//   }
+// }
+
+
+// export default characters;
+
+// import { ADD_CHARACTER } from "../actions";
+// import characters_json from '../data/characters.json'
+
+// function characters(state = characters_json, action) {
+//   switch(action.type) {
+//     case ADD_CHARACTER:
+//       default:
+//         return state;
+//   }
+// }
+
+// export default characters;
+
+
+import { ADD_CHARACTER } from "../actions";
 import characters_json from '../data/characters.json'
-import { ADD_CHARACTER } from '../actions/index.js'
 
 function characters(state = characters_json, action) {
   switch(action.type) {
-    case ADD_CHARACTER:
-      let characters = state.filter(item => item.id !== action.id);
-      return characters;  
     default:
-        return state;
-  }  
+      return state;
+  }
 }
 
-export default characters;
 
+export default characters
