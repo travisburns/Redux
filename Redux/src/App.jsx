325dfc576cc5d7ab650865799d4231af5fc36137
Travis <travisjb0@gmail.com>
2023-11-07 12:56:34 -0800

    Added and practiced store. Sort of understand it, estimated understanding of 25 - 30%

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -4,15 +4,15 @@ import './App.css'
 
 
 import {createStore} from 'redux'
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux'  //Provider should be surrounding the app, this allows all props to be utilized across all components
 
 import characters from './reducers/index.js'
 import { addCharacterById } from './actions/index.js'
 
-const store = createStore(characters)
+
 
 function App() {
-  const [count, setCount] = useState(0)
+const store = createStore(characters)
 console.log('store.getState()', store.getState());
 store.subscribe(() => console.log('store', store.getState()));
 store.dispatch(addCharacterById(2))
@@ -30,3 +30,28 @@ store.dispatch(addCharacterById(2))
 }
 
 export default App
+
+
+
+
+// const store = createStore(characters)
+
+// function App() {
+//   const [count, setCount] = useState(0)
+// console.log('store.getState()', store.getState());
+// store.subscribe(() => console.log('store', store.getState()));
+// store.dispatch(addCharacterById(2))
+
+//   return (
+//     <Provider store={store}>
+//  <>
+//       <div>
+//         </div>
+       
+//     </>
+//     </Provider>
+   
+//   )
+// }
+
+// export default App
