0255ff79b66939958880f5eaea7462a93b585014
Travis <travisjb0@gmail.com>
2023-11-12 21:30:34 -0800

    Added edits to rootlayout

--- Redux/src/components/RootLayout.jsx
+++ Redux/src/components/RootLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import DashBoard from './DashBoard'
 
 
 const RootLayout = () => {
   return (
     
       <main>
+        <DashBoard />
       <Outlet />
       </main>
       
