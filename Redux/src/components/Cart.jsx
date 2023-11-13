61cda952f99801a08ca7816b8d9516036fcb5e29
Travis <travisjb0@gmail.com>
2023-11-13 14:59:39 -0800

    Correctly was able to use useSelector and dispatch to add the selected products to the cart and correctly display them in the dom

--- Redux/src/components/Cart.jsx
+++ Redux/src/components/Cart.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import Card from 'react-bootstrap/Card';
+
 
 const Cart = () => {
-  return (
-    <div>
-      <h1>Cart</h1>
+  const products = useSelector(state => state.cart);
+
+  const cards = products.map(product => (
+    <div className='col-md-12' style={{ marginBottom: "10px"}}>
+      <Card key={product.id} className='h-100'>
+        <div>
+        <Card.Img variant="top" src={product.image}  style={{ width : '100px', height: '130px'}}/>
+        </div>
+       
+        <Card.Body>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>
+             USD: {product.price}$
+            </Card.Text>
+            
+
+
+        </Card.Body>
+      <Card.Footer style={{ background : "white"}}>
+      
+      </Card.Footer>
+      </Card>
     </div>
+  ))
+
+
+  return (
+    <>
+    <div className='row'>
+    {cards}
+
+    </div>
+    </>
   )
 }
 
