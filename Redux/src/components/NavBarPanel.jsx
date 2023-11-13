0245b866b3fc9fcc6c3ea5e4f59dc5c6d58a589c
Travis <travisjb0@gmail.com>
2023-11-13 14:50:50 -0800

    Added a useSelector to acess the dispatch data and set it on navlink

--- Redux/src/components/NavBarPanel.jsx
+++ Redux/src/components/NavBarPanel.jsx
@@ -5,10 +5,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 
 
 const NavBarPanel = () => {
+    const cartProducts = useSelector(state => state.cart);
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
@@ -24,7 +26,7 @@ const NavBarPanel = () => {
          <Navbar.Toggle />
          <Navbar.Collapse className='justify-content-end'>
             <Navbar.Text>
-            <Nav.Link to="/cart" as={Link}>My bag 0</Nav.Link>
+            <Nav.Link to="/cart" as={Link}>My bag {cartProducts.length}</Nav.Link>
             </Navbar.Text>
          </Navbar.Collapse>
         
