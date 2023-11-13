1482048d39627a3423ffb969cbad69d50ae8ad66
Travis <travisjb0@gmail.com>
2023-11-12 18:18:03 -0800

    Was able to fetch the product data from the url using a useEffect hook. From here I was map the products and retrieve the product.image from the products all while using react bootstrap to display the card type and UI

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
--- Redux/src/components/Product.jsx
+++ Redux/src/components/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Card from 'react-bootstrap/Card'
 import {useState, useEffect} from 'react'
 
 const Product = () => {
@@ -14,11 +14,26 @@ const Product = () => {
   }, []);
 
 
-
+  const cards = products.map(product => (
+    <div className='col-md-3'>
+      <Card style={{ width: '18rem'}}>
+        <Card.Img variant="top" src={product.image} />
+        <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </Card.Text>
+            <button variant="primary"> Go somewhere</button>
+        </Card.Body>
+      </Card>
+    </div>
+  ))
   return (
     <div>
-      <h1>PRODUCT!!!</h1>
-      {JSON.stringify(products)}
+      <h1>Product Dashboard</h1>
+      <div className='row'>
+        {cards}
+      </div>
     </div>
   )
 }
