407e94dbe2a3a61797f0a29fd1086a009c17e38e
Travis <travisjb0@gmail.com>
2023-11-14 13:24:26 -0800

    Redux thunk used, however, thunk method from tutorial is outdated and person doesnt update course soleft in a rut, but 90 percent of redux with react I beleive I understand. Can go to eccomerce site now

--- Redux/src/components/Product.jsx
+++ Redux/src/components/Product.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import {useState, useEffect} from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import {add} from '../store/cartSlice';
+import  {getProducts}  from '../store/productSlice';
+
 
 const Product = () => {
   const dispatch = useDispatch();
-
-  const [products, getProducts] = useState([]);
-
+  const {data: products} = useSelector(state => state.products);
+  console.log(products)
   useEffect(() => {
     //api
-    fetch('https://fakestoreapi.com/products')
-    .then(data => data.json())
-    .then(result => getProducts(result))
+
+    //dispatch an action for fetchProducts
+    dispatch(getProducts());
+    // fetch('https://fakestoreapi.com/products')
+    // .then(data => data.json())
+    // .then(result => getProducts(result))
+
+
   }, []);
 
   const addToCart = (product) => {
     dispatch(add(product))
   }
 
+ 
   const cards = products.map(product => (
     <div className='col-md-3' style={{ marginBottom: "10px"}}>
       <Card key={product.id} className='h-100'>
